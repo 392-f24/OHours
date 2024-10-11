4ad57a7fc0f051457d8726f8e2240b20e1483a87
TerryChenRDFZ <terrylinhaochen@163.com>
2024-10-11 13:50:50 -0500

    firebase adding dist

---
+++ OHours/dist/assets/back-BLN-uxUa.js
@@ -0,0 +1,6 @@
+import{j as e,L as t}from"./index-CF3V3EkY.js";import{c as n}from"./createLucideIcon-BVwf0bP4.js";/**
+ * @license lucide-react v0.447.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=n("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]);function a(){return e.jsxs(t,{to:"..",relative:"path",className:"inline-flex items-center gap-2 px-4 py-2 text-sm font-medium text-gray-700 transition-colors rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200",children:[e.jsx(o,{className:"w-4 h-4"}),"Back"]})}export{a as B};
