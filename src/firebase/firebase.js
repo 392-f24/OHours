d8f024a7f324d64ccfb59cf9b1fea6d907420dba
Lianhao Zheng <lianhaoz06@gmail.com>
2024-11-07 00:33:23 -0600

    checking

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,7 +5,7 @@ import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
