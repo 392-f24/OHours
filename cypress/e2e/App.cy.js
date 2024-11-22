0d9e16389916df7fee59ba9415ec671ced3f35bc
jannalee0819 <JannaLee2025@u.northwestern.edu>
2024-11-22 15:47:46 -0600

    basic content + nav testing

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,9 +1,42 @@
 /* globals cy */
     
 describe ('Test App', () => {
-
-    it ('launches', () => {
-      cy.visit ('/');
+    beforeEach(() => {
+        cy.visit('/');
+      });
+    
+      it('launches', () => {
+        // Basic launch test is already covered by beforeEach
+        cy.url().should('eq', Cypress.config().baseUrl + '/');
+      });
+    
+      it('shows Welcome to OHours', () => {
+        // Test main heading
+        cy.get('h1')
+          .should('contain', 'Welcome to')
+          .and('contain', 'OHours');
+        
+        // Test subtitle
+        cy.get('p')
+          .should('contain', 'Office Hours scheduling made easy');
+      });
+    
+      it('takes user to pm landing when staff is clicked', () => {
+        cy.get('a[href="/PMLand"]').click();
+        cy.url().should('include', '/PMLand');
+      });
+    
+      it('takes user to student landing when student is clicked', () => {
+        cy.get('a[href="/student"]').click();
+        cy.url().should('include', '/student');
+      });
+    
+      it('displays staff and student navigation cards', () => {
+        // Test Staff card
+        cy.get('a[href="/PMLand"]').within(() => {
+          cy.get('svg').should('exist'); // Check for Users icon
+          cy.get('span').should('contain', 'Staff');
+        });
     });
   
   });
